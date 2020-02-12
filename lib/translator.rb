f744ce31581deed90126bf8af796aa37b2a17700
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-12 19:05:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,8 +38,10 @@ def load_library(file_path)
   emoticons_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon_hash)
+  
+  
+  
 end
 
 def get_english_meaning
