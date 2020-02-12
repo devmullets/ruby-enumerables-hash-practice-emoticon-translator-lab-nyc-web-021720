27ce6a7a5a26f533293e6fdab0c1f7e5da748d7b
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-12 19:24:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -43,9 +43,7 @@ def get_japanese_emoticon(file_path, english_emoji)
   emoticons_hash = load_library(file_path)
   
   if emoticons_hash[:get_emoticon].has_key?(english_emoji)
-    # found_emoji = emoticons_hash[:get_emoticon][english_emoji]
-    # puts "I think we found the key here: #{emoticons_hash[:get_emoticon][english_emoji]}"
-    # puts "Found the key"
+    # look up english emoji and see 
     emoticons_hash[:get_emoticon][english_emoji]
   else 
     return "Sorry, that emoticon was not found"
